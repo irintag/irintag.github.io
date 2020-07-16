0021da8f288a67b01cc82837eca3015deac3a637
irintag <irinatagaevarene@mail.ru>
2020-07-16 15:12:19 +0600

    Add files via upload

---
+++ build/precache-manifest.820af1c97c64277303bc6878ec5897b8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2979930c7eaa8f9e5ae675bd20abd7c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6afab3ece1baf60328ff",
+    "url": "/static/css/main.534e04d6.chunk.css"
+  },
+  {
+    "revision": "780ea772aca5a9b7f90d",
+    "url": "/static/js/2.52e2f0de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.52e2f0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6afab3ece1baf60328ff",
+    "url": "/static/js/main.65e9c283.chunk.js"
+  },
+  {
+    "revision": "ea4fdd6315ab32d12f90",
+    "url": "/static/js/runtime-main.ad928bad.js"
+  },
+  {
+    "revision": "bda976c9515f505c7de57ec20629319a",
+    "url": "/static/media/Black.bda976c9.png"
+  },
+  {
+    "revision": "d2edb5472648df2d97aff76dc097b24f",
+    "url": "/static/media/Logo1.d2edb547.png"
+  },
+  {
+    "revision": "2715cbe4e3941c1f123644d5d8a74492",
+    "url": "/static/media/Rectangle 14.2715cbe4.png"
+  },
+  {
+    "revision": "540407a05850cacb0213086ed98bedef",
+    "url": "/static/media/Rectangle 44.540407a0.png"
+  },
+  {
+    "revision": "0ca5f6f9af8c74a206f15977388d575a",
+    "url": "/static/media/Rectangle 47.0ca5f6f9.png"
+  },
+  {
+    "revision": "0c2b51c7938ec2d98f327729911be3f3",
+    "url": "/static/media/Rectangle43.0c2b51c7.png"
+  },
+  {
+    "revision": "92d6d134f44fbc4b497a31a340b84945",
+    "url": "/static/media/Section2.92d6d134.png"
+  },
+  {
+    "revision": "6a89c100af9a94909613d07256f71e1f",
+    "url": "/static/media/map.6a89c100.png"
+  }
+]);
